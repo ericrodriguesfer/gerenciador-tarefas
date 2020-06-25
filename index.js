407f3a02fdd481f9cdc0f909b4a014355daee2de
ericrodriguesfer <er601432@gmail.com>
2020-06-25 14:54:46 -0300

    realizando os teste de sessão para o login

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const LocalStorage = require("node-localstorage").LocalStorage;
+const localStorage = new LocalStorage("./scratch");
 const app = express();
 
 app.use(express.static('public/'));
@@ -8,15 +10,20 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get("/", function(require, response){
+    localStorage.clear();
     response.render("login/index");
 });
 
 app.get("/register/user", function(require, response){
+    localStorage.setItem('logon', true);
     response.render("register/user/index");
 });
 
 app.get("/home", function(require, response){
-    response.render("home/index");
+    if(localStorage.getItem('logon'))
+        response.render("home/index");
+    else 
+        response.render("login/index");
 });
 
 app.get("/register/activity", function(require, response){
